9e84c6f5e5be8c651516a0f40c640f45a5917d2e
Felix <jonfelixrico@gmail.com>
2022-07-13 17:36:12 +0800

    Make the endpoint more flexible

--- src/main/java/com/example/demo/controller/PdfPocController.java
+++ src/main/java/com/example/demo/controller/PdfPocController.java
@@ -5,6 +5,7 @@ package com.example.demo.controller;
 
 import java.io.IOException;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +18,11 @@ import com.lowagie.text.pdf.parser.PdfTextExtractor;
  */
 @RestController
 public class PdfPocController { 
-	@RequestMapping("/")
-	public String getTextTest() throws IOException {
-		PdfReader reader = new PdfReader("files\\target.pdf");
+	@RequestMapping("/{fileName}/{pageNo}")
+	public String getTextTest(@PathVariable String fileName, @PathVariable String pageNo) throws IOException {
+		int parsedPageNo = Integer.parseInt(pageNo);
+		PdfReader reader = new PdfReader(String.format("files/%s.pdf", fileName));
 		PdfTextExtractor extractor = new PdfTextExtractor(reader);
-		return extractor.getTextFromPage(1);
+		return extractor.getTextFromPage(parsedPageNo);
 	}
 }
