991abb8e8b3f76e16711f9037eb7d533a64fe705
Felix <jonfelixrico@gmail.com>
2022-07-14 13:01:05 +0800

    Exclude pages/docs without matches

--- src/main/java/com/example/demo/service/pdf/PageResult.java
+++ src/main/java/com/example/demo/service/pdf/PageResult.java
@@ -6,13 +6,20 @@ import org.springframework.data.util.Pair;
 
 public class PageResult {
 	private List<Pair<Integer, Integer>> hits;
-
-	public PageResult(List<Pair<Integer, Integer>> hits) {
-		super();
+	private int pageNo;
+	
+	public PageResult(int pageNo, List<Pair<Integer, Integer>> hits) {
 		this.hits = hits;
+		this.pageNo = pageNo;
 	}
 
 	public List<Pair<Integer, Integer>> getHits() {
 		return hits;
 	}
+
+	public int getPageNo() {
+		return pageNo;
+	}
+	
+	
 }
--- src/main/java/com/example/demo/service/pdf/PdfService.java
+++ src/main/java/com/example/demo/service/pdf/PdfService.java
@@ -27,11 +27,10 @@ public class PdfService {
 				.collect(Collectors.toList());
 	}
 	
-	private List<String> getTextContentOfPdf(String path) throws IOException {
-		PdfReader reader = new PdfReader(path);
+	private List<String> getTextContentFromPdfReader(PdfReader reader) {
 		PdfTextExtractor extractor = new PdfTextExtractor(reader);
 
-		return IntStream.range(1, reader.getNumberOfPages() + 1)
+		return IntStream.range(1, reader.getNumberOfPages() + 1) // 2nd arg is exclusive, hence the +1
 				.mapToObj(pageNo -> {
 					try {
 						return extractor.getTextFromPage(pageNo);
@@ -49,34 +48,54 @@ public class PdfService {
 		return matcher.results().map(result -> Pair.of(result.start(), result.end())).collect(Collectors.toList());
 	}
 	
+	private List<PageResult> getPageResultsFromPdfReader(PdfReader reader, String searchTerm) {
+		List<String> textContentList = getTextContentFromPdfReader(reader);
+		
+		// includes only pages with matches
+		List<PageResult> results = new ArrayList<>();
+		for (int i = 0; i < textContentList.size(); i++) {
+			List<Pair<Integer, Integer>> matches = getMatchIndexes(textContentList.get(i), searchTerm);
+			if (matches.size() == 0) {
+				continue; // do not include pages without matches
+			}
+			
+			results.add(new PageResult(i + 1, matches));
+		}
+		
+		return results;
+	}
+	
+	private SearchResult getSearchResult(String filename, String searchTerm) throws IOException {
+		PdfReader reader = new PdfReader(String.format("%s/%s", DIRECTORY_ROOT, filename));
+		List<PageResult> results = getPageResultsFromPdfReader(reader, searchTerm);
+		
+		if (results.size() == 0) {
+			return null;
+		}
+		
+		return new SearchResult(filename, results);
+	}
+	
 	private static final String DIRECTORY_ROOT = "files";
 	
 	public List<SearchResult> searchAllFiles(String searchTerm) {
 		Stream<String> paths = getAllPdfsInDirectory(DIRECTORY_ROOT).stream();
 		
-		return paths.map(filename -> {
-			List<PageResult> results;
-			try {
-				results = getTextContentOfPdf(String.format("%s/%s", DIRECTORY_ROOT, filename))
-					.stream()
-					.map(textContent -> new PageResult(getMatchIndexes(textContent, searchTerm)))
-					.collect(Collectors.toList());
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				results = new ArrayList<>();
-			}
-
-			return new SearchResult(filename, results);
-		}).collect(Collectors.toList());
+		return paths
+				.map(filename -> {
+					try {
+						return getSearchResult(filename, searchTerm);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						return null;
+					}
+				})
+				.filter(result -> result != null)
+				.collect(Collectors.toList());
 	}
 	
 	public SearchResult searchFile(String searchTerm, String filename) throws IOException {
-		List<PageResult> results = getTextContentOfPdf(String.format("%s/%s.pdf", DIRECTORY_ROOT, filename))
-				.stream()
-				.map(textContent -> new PageResult(getMatchIndexes(textContent, searchTerm)))
-				.collect(Collectors.toList());
-		
-		return new SearchResult(filename, results);
+		return getSearchResult(String.format("%s.pdf", filename), searchTerm);
 	}
 }
